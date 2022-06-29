8aacf51179bb280e39cd758a44c9b90cecb2420e
Carlos Tandazo <carlosatandazo@gmail.com>
2022-06-28 19:34:35 -0500

    taller

--- taller/taller/residencias/models.py
+++ taller/taller/residencias/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Edificio(models.Model):
+    
+	opcionesTipo = (
+		('residencial', 'Residencial'),
+		('comercial', 'Comercial'),
+	)
+	nombre = models.CharField(max_length=30)
+	direccion = models.CharField(max_length=30)
+	ciudad = models.CharField(max_length=30)
+	tipo = models.CharField(max_length=25, choices=opcionesTipo)
+
+	def __str__(self):
+		return "%s %s %s %s" % (self.nombre,
+			self.direccion,
+			self.ciudad,
+			self.tipo)
+
+class Departamento(models.Model):
+    nombrePropietario = models.CharField(max_length=100)
+    costo = models.DecimalField(max_digits=100, decimal_places=2)
+    nroCuartos = models.IntegerField('Numero de cuartos')
+    edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE,
+            related_name="departamentos")
+
+    def __str__(self):
+        return "%s %.2f %d" % (self.nombrePropietario, 
+        	self.costo,
+        	self.nroCuartos)
+
+
